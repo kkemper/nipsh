fac76e967cc1b5bcbd500f01cf8a34435c6de81a
Kit Kemper <cgkemper@gmail.com>
2016-08-03 11:23:26 -0400

    Initial draft-pass on static pages (Home, Help, About, Contact). Bootstrap implementation. Routes.

--- NIPSH/config/routes.rb
+++ NIPSH/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/contact'
+  get 'about' => 'static_pages#about'
 
-  root 'application#hello'
+  get 'contact' => 'static_pages#contact'
   
   get 'fitness_tests/new'
 

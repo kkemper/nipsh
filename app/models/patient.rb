725eeac04a7b5dc7a547c903befd32ee77b987e1
Kit Kemper <cgkemper@gmail.com>
2017-01-03 20:51:38 -0500

    Updated patient.rb model adding through relationships for all other mds report items.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,17 @@ class Patient < ActiveRecord::Base
 	has_many :lab_data
 	has_many :physical_exams
   has_many :fitness_tests, through: :mds_reports
+  has_many :cancer_screenings, through: :mds_reports
+  has_many :current_jobs, through: :mds_reports
+  has_many :demographics, through: :mds_reports
+  has_many :duties, through: :mds_reports
+  has_many :family_histories, through: :mds_reports
+  has_many :health_conditions, through: :mds_reports
+  has_many :hearing_tests, through: :mds_reports
+  has_many :immunizations, through: :mds_reports
+  has_many :injury_illnesses, through: :mds_reports
+  has_many :other_employments, through: :mds_reports
+  has_many :physical_activities, through: :mds_reports
+  has_many :surgeries, through: :mds_reports
+  has_many :tobacco_and_alcohols, through: :mds_reports
 end

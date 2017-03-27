f9fa921f1c8da14259a2ba5f896b49b749b4825a
Kit Kemper <cgkemper@gmail.com>
2017-03-27 15:34:45 -0400

    Updated patients with MDS associations.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,29 @@
 class Patient < ActiveRecord::Base
-	has_many :mds_reports
-	has_many :lab_data
-	has_many :physical_exams
+	has_one :mds_patient
+  has_one :bsdi_patient
+  has_many :bsdi_reports, through: :bsdi_patient
+  has_many :mds_reports, through: :mds_patient
+  has_many :bsdi_blood_pressures, through: :bsdi_reports
+  has_many :bsdi_blood_chemistries, through: :bsdi_reports
+  has_many :bsdi_activities, through: :bsdi_reports
+  has_many :bsdi_alcohol_barriers, through: :bsdi_reports
+  has_many :bsdi_annuals, through: :bsdi_reports
+  has_many :bsdi_smokings, through: :bsdi_reports
+  has_many :bsdi_current_jobs, through: :bsdi_reports
+  has_many :bsdi_diet_barriers, through: :bsdi_reports
+  has_many :bsdi_diets, through: :bsdi_reports
+  has_many :bsdi_disease, through: :bsdi_reports
+  has_many :bsdi_exercises, through: :bsdi_reports
+  has_many :bsdi_histories, through: :bsdi_reports
+  has_many :bsdi_hosps, through: :bsdi_reports
+  has_many :bsdi_measurements, through: :bsdi_reports
+  has_many :bsdi_risks, through: :bsdi_reports
+  has_many :bsdi_psyches, through: :bsdi_reports
+  has_many :bsdi_smoking_barriers, through: :bsdi_reports
+  has_many :bsdi_tobacco_and_alcohols, through: :bsdi_reports
+  has_many :bsdi_weights, through: :bsdi_reports
+  has_many :lab_data, through: :mds_patient
+  has_many :physical_exams, through: :mds_patient
   has_many :fitness_tests, through: :mds_reports
   has_many :cancer_screenings, through: :mds_reports
   has_many :current_jobs, through: :mds_reports
@@ -16,11 +38,6 @@ class Patient < ActiveRecord::Base
   has_many :physical_activities, through: :mds_reports
   has_many :surgeries, through: :mds_reports
   has_many :tobacco_and_alcohols, through: :mds_reports
-  has_one :bsdi_patient
-  has_many :bsdi_reports, through: :bsdi_patient
-  has_many :bsdi_blood_pressures, through: :bsdi_reports
-  has_many :bsdi_blood_chemistries, through: :bsdi_reports
-  has_many :bsdi_smokings, through: :bsdi_reports
 
 private
 

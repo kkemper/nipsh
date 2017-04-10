0da52f0350f6dfd5c7eb81219f645e7889cea4da
Kit Kemper <cgkemper@gmail.com>
2017-04-10 13:02:37 -0400

    Changed fitness_rest_dbp to float.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410165505) do
+ActiveRecord::Schema.define(version: 20170410170059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -450,7 +450,6 @@ ActiveRecord::Schema.define(version: 20170410165505) do
     t.integer  "fitness_crunches"
     t.integer  "fitness_q_3"
     t.integer  "fitness_q_4"
-    t.integer  "fitness_rest_hr"
     t.integer  "fitness_rest_sbp"
     t.integer  "fitness_rest_dbp"
     t.integer  "fitness_rest_ecg"
@@ -500,6 +499,7 @@ ActiveRecord::Schema.define(version: 20170410165505) do
     t.float    "fitness_age"
     t.float    "fitness_max_dbp"
     t.integer  "fitness_overread"
+    t.float    "fitness_rest_hr"
   end
 
   create_table "fitness_tests", force: :cascade do |t|

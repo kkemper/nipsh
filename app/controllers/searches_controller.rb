e41f4200b2a220ec25643aa322090937badd2a51
Kit Kemper <cgkemper@gmail.com>
2017-04-27 17:35:28 -0400

    removed all ransackable attribuites from the searches controler except for bsdi_blood_chemistries, bsdi_blood_pressures, and bsdi_smokings.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
   include SearchesHelper
 
   def index
-    @search = Patient.includes(:bsdi_activities, :bsdi_alcohol_barriers, :bsdi_annuals, :bsdi_blood_chemistries, :bsdi_blood_pressures, :bsdi_current_jobs, :bsdi_diets, :bsdi_diseases, :bsdi_exercises, :bsdi_histories, :bsdi_hosps, :bsdi_measurements, :bsdi_patient, :bsdi_psyches, :bsdi_reports, :bsdi_risks, :bsdi_smokings, :bsdi_tobacco_and_alcohols, :bsdi_weights).ransack(params[:q])
+    @search = Patient.includes(:bsdi_blood_chemistries, :bsdi_blood_pressures, :bsdi_smokings).ransack(params[:q])
     @results = @search.result(distinct: true)
     @search.build_condition if @search.conditions.empty?
     @search.build_sort if @search.sorts.empty?

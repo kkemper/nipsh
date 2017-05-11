217bbcd0b4182ac9a8329feac2773050a31b6f9f
Kit Kemper <cgkemper@gmail.com>
2017-05-11 13:25:26 -0400

    Removed gender and employeeid from mds_patients. employeeid will become id. Gender resides in the demographgics table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511165850) do
+ActiveRecord::Schema.define(version: 20170511172122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -663,8 +663,6 @@ ActiveRecord::Schema.define(version: 20170511165850) do
     t.string   "lname"
     t.string   "fname"
     t.string   "mname"
-    t.integer  "gender"
-    t.string   "employeeid"
     t.string   "ssn"
     t.datetime "created_at", default: "now()", null: false
     t.datetime "updated_at", default: "now()", null: false

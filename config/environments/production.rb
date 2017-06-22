dfd8676e076e603d5c59608e8c9fea0debbe03df
Kit Kemper <cgkemper@gmail.com>
2017-06-22 14:27:09 -0400

    Added commas to config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-  address:      ‘smtp.sendgrid.net’
-  domain:     ‘NIPSH’
+  address:      ‘smtp.sendgrid.net’,
+  domain:     ‘NIPSH’,
   user_name:    ‘ENV[“EMAILUSERNAME”],
   password:     ‘ENV[“EMAILPASSWORD”],
   port:       587,

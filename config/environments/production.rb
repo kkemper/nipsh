34b07c7d8d2251bf66c3f2dcf143db4ac19ca22a
Kit Kemper <cgkemper@gmail.com>
2017-06-22 14:41:07 -0400

    Changed single quotations in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-  :address      =>  ‘smtp.sendgrid.net’,
+  :address      =>  'smtp.sendgrid.net',
   :domain       =>  ‘NIPSH’,
   :user_name    =>  ‘ENV[“EMAILUSERNAME”],
   :password     =>  ‘ENV[“EMAILPASSWORD”],

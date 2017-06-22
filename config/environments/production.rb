883e0f9ee4a0ecd0b87e29013f8197e08a0c8d24
Kit Kemper <cgkemper@gmail.com>
2017-06-22 14:43:12 -0400

    Fixed mor ehyphen issues.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
   :address      =>  'smtp.sendgrid.net',
-  :domain       =>  ‘NIPSH’,
-  :user_name    =>  ‘ENV[“EMAILUSERNAME”],
-  :password     =>  ‘ENV[“EMAILPASSWORD”],
+  :domain       =>  'NIPSH',
+  :user_name    =>  ENV[“EMAILUSERNAME”],
+  :password     =>  ENV[“EMAILPASSWORD”],
   :port         =>  '587',
   :authentication  =>  :plain,
   :enable_starttls_auto => true  

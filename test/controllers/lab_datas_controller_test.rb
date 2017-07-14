abb41c284ac48ce60172b232e6dbb9f2cccccef9
Kit Kemper <cgkemper@gmail.com>
2017-07-14 13:15:32 -0400

    Removed controllers, fixtures, and models from test folder for hearing_tests, immunizations, lab_data, fitness_tests, and physical_exams.

--- test/controllers/lab_datas_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LabDatasControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/physical_exams_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PhysicalExamsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-end
